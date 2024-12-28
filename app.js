c3f941e2af2a928eafdc894a09ce6178f374b0b1
min khant <minkhant4287@gmail.com>
2024-12-28 23:24:30 +0630

    fixing UI Design

--- app.js
+++ app.js
@@ -16,31 +16,35 @@ const anniCalculate = (date1, date2) => {
     let monthCheck = month.checked;
     let diff = Math.floor(date2.getTime() - date1.getTime());
     let day = 1000 * 60 * 60 * 24;
-    let days = Math.round(diff / day);
+    let days = Math.floor(diff / day);
     let months = Math.round(days / monthDays[monthDay]);
     let years = Math.floor(months / 12);
     months = months > 12 ? months -= 12 : months
     if (yearCheck && monthCheck) {
         anniversary = `
         <h1>Happy Anniversary Day</h1>
+        <img src="./images/In love-pana.png" alt="love1" class="img1">
         <p>It has been ${years} year and ${months} months and ${days} days since you started your relationship.</p>
        <a href="index.html">Back</a>
         `;
     } else if (yearCheck) {
         anniversary = `
         <h1>Happy Anniversary Day</h1>
+         <img src="./images/In love-pana.png" alt="love1" class="img1">
         <p>It has been ${years} years and ${days} days since you started your relationship.</p>
         <a href="index.html">Back</a>
         `;
     } else if (monthCheck) {
         anniversary = `
         <h1>Happy Anniversary Day</h1>
+         <img src="./images/In love-pana.png" alt="love1" class="img1">
         <p>It has been ${months} months and ${days} days since you started your relationship.</p>
         <a href="index.html">Back</a>
         `;
     } else {
         anniversary = `
         <h1>Happy Anniversary Day</h1>
+        <img src="./images/In love-pana.png" alt="love1" class="img1">
         <p>It has been ${days} days since you started your relationship.</p>
         <a href="index.html">Back</a>
         `;
@@ -52,9 +56,3 @@ searhBtn.addEventListener("click", () => {
     let past = new Date(anniDate);
     anniCalculate(past, today);
 });
-// *
-
-// anniversary = `
-// <h1>Happy Anniversary Day</h1>
-// <p>It has been  days since you started your relationship.</p>
-// `
